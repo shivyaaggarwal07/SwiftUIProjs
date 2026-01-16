ed6117e1addc03b8c89239a57a715951717391e8
shivyaaggarwal07 <41586393+shivyaaggarwal07@users.noreply.github.com>
2026-01-16 14:18:31 +0530

    Add files via upload
    updated code with core data and color extensions

---
+++ TMMHealthDashboard/ColorExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  ColorExtensions.swift
+//  TMMHealthDashboard
+//
+//  Created by Shivya Aggarwal on 16/01/26.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let cardBackground = Color(uiColor: .secondarySystemBackground)
+    static let appBackground = Color(uiColor: .systemGroupedBackground)
+    static let primaryText = Color(uiColor: .label)
+    static let secondaryText = Color(uiColor: .secondaryLabel)
+}
